781070098122489bca067e1906c9136aa00ada59
Pedro Emanuel <pedroemanuelvitorin00@gmail.com>
2026-08-29 14:34:40 -0300

    Adicionado todos os templates .html, criando estruturas inicial das views, configurando urls

--- RuyFrotas/urls.py
+++ RuyFrotas/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    ]
+    path("veiculos/", views.veiculos, name="veiculos"),
+    path("motoristas/", views.motoristas, name="motoristas"),
+    path("rotas", views.rotas, name="rotas"),
+    path("solicitacoes", views.solicitacoes, name="solicitacoes"),
+    path("manutencoes/", views.manutencoes, name="manutencoes"),
+    path("gastos/", views.gastos, name="gastos"),
+]
