250966a12cf1dec52f199ed3b0a7f4c7e0ae1c2d
Pedro Emanuel <pedroemanuelvitorin00@gmail.com>
2026-08-31 11:44:14 -0300

    Adicionando views e urls das páginas de detalhar, configurando as páginas de ver_html

--- RuyFrotas/urls.py
+++ RuyFrotas/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path("solicitacoes", views.solicitacoes, name="solicitacoes"),
     path("manutencoes/", views.manutencoes, name="manutencoes"),
     path("gastos/", views.gastos, name="gastos"),
+    path("veiculos/<int:id_veiculo>/", views.ver_veiculos, name="detalhe_veiculo"),
+    path("motoristas/<int:id_motorista>/", views.ver_motoristas, name="detalhe_motorista"),
+    path("rotas/<int:id_rotas>/", views.ver_rotas, name="detalhe_rota"),
+    path("solicitacoes/<int:id_solicitacao>/", views.ver_solicitacao, name="detalhe_solicitacao"),
+    path("manutencoes/<int:id_manutencao>/", views.ver_manutencoes, name="detalhe_manutencao"),
+    path("gastos/<int:id_gastos>/", views.ver_gastos, name="detalhe_gasto"),
 ]
--- RuyFrotas/views.py
+++ RuyFrotas/views.py
@@ -1,23 +1,79 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Motorista, Veiculo, Rota, Solicitacao, Manutencao,Gasto, Viagem
 
 def index(request):
     return render(request,"RuyFrotas/index.html")
 
 def veiculos(request):
-    return render(request,"RuyFrotas/veiculos.html")
+    context = {
+        "veiculos": Veiculo.objects.all(),
+    }
+    return render(request,"RuyFrotas/veiculos.html", context)
+
+def ver_veiculos(request, id_veiculo):
+    context = {
+        "veiculo": get_object_or_404(Veiculo, id=id_veiculo),
+    }
+    return render(request, "RuyFrotas/veiculo_ver.html", context)
 
 def motoristas(request):
-    return render(request,"RuyFrotas/motoristas.html")
+
+    context = {
+            "motoristas": Motorista.objects.all(),
+        }
+    return render(request,"RuyFrotas/motoristas.html", context)
+
+
+def ver_motoristas(request, id_motorista):
+    context = {
+        "motorista": get_object_or_404(Motorista, id=id_motorista),
+    }
+    return render(request, "RuyFrotas/motorista_ver.html", context)
 
 def rotas(request):
-    return render(request,"RuyFrotas/rotas.html")
+    context = {
+            "rotas": Rota.objects.all(),
+        }
+    return render(request,"RuyFrotas/rotas.html",context)
+
+def ver_rotas(request, id_rotas):
+    context = {
+        "rota": get_object_or_404(Rota, id=id_rotas),
+    }
+    return render(request, "RuyFrotas/rota_ver.html", context)
 
 def solicitacoes(request):
-    return render(request,"RuyFrotas/solicitacoes.html")
+    context = {
+            "solicitacoes": Solicitacao.objects.all(),
+        }
+    return render(request,"RuyFrotas/solicitacoes.html",context)
+
+def ver_solicitacao(request, id_solicitacao):
+    context = {
+        "solicitacao": get_object_or_404(Solicitacao, id=id_solicitacao),
+    }
+    return render(request, "RuyFrotas/solicitacao_ver.html", context)
 
 def manutencoes(request):
-    return render(request,"RuyFrotas/manutencoes.html")
+    context = {
+            "manutencoes": Manutencao.objects.all(),
+        }
+    return render(request,"RuyFrotas/manutencoes.html",context)
+
+def ver_manutencoes(request, id_manutencao):
+    context = {
+        "rota": get_object_or_404(Manutencao, id=id_manutencao),
+    }
+    return render(request, "RuyFrotas/manutencao_ver.html", context)
 
 def gastos(request):
-    return render(request,"RuyFrotas/gastos.html")
+    context = {
+            "gastos": Gasto.objects.all(),
+        }
+    return render(request,"RuyFrotas/gastos.html",context)
 
+def ver_gastos(request, id_gastos):
+    context = {
+        "rota": get_object_or_404(Gasto, id=id_gastos),
+    }
+    return render(request, "RuyFrotas/gasto_ver.html", context)
