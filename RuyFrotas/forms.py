b6df9e95319149c77388abe67e254235f2a39bd1
Nicolly Silva <nicollysilvagomesdelima@gmail.com>
2026-09-24 11:55:41 -0300

    adicionando os forms

---
+++ RuyFrotas/forms.py
@@ -0,0 +1,42 @@
+from django import forms
+from .models import Usuario, Motorista, Veiculo, Rota, Solicitacao, Manutencao, Gasto, Viagem
+
+class FormsUsuario(forms.ModelForm):
+    class Meta:
+        model = Usuario
+        fields = "__all__"
+
+class FormsMotorista(forms.ModelForm):
+    class Meta:
+        model = Motorista
+        fields = "__all__"
+
+class FormsVeiculo(forms.ModelForm):
+    class Meta:
+        model = Veiculo
+        fields = "__all__"
+
+class FormsRota(forms.ModelForm):
+    class Meta:
+        model = Rota
+        fields = "__all__"
+
+class FormsSolicitacao(forms.ModelForm):
+    class Meta:
+        model = Solicitacao
+        fields = "__all__"
+
+class FormsManutencao(forms.ModelForm):
+    class Meta:
+        model = Manutencao
+        fields = "__all__"
+
+class FormsGasto(forms.ModelForm):
+    class Meta:
+        model = Gasto
+        fields = "__all__"
+
+class FormsViagem(forms.ModelForm):
+    class Meta:
+        model = Viagem
+        fields = "__all__"
